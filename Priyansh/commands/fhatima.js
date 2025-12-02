5aa3334145d4ad73fdcfb5a5a73ab1bbb1cc6e75
rahulrahul58844 <rahulkumar424324@gmail.com>
2025-12-02 22:39:15 +0530

    Add files via upload

---
+++ Priyansh/commands/fhatima.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "fhatima",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Arun", 
+	description: "no prefix",
+	commandCategory: "No command marks needed",
+	usages: "...",
+    cooldowns: 100, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("boss") ||
+     react.includes("virat") || 
+react.includes("owner")) {
+		var msg = {
+				body: "Fhatima mullo ki bahan hai😠😠",attachment: fs.createReadStream(__dirname + `/noprefix/owner.jpeg`)
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+	}
